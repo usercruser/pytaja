71b28259e81fd3959703d5ba292318ebdc105cd4
p0pl99 <80199718+usercruser@users.noreply.github.com>
2025-02-25 14:41:56 +0900

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,7 +1,14 @@
+winget install python3 -s winget
+winget install python-pip
 python3 -m pip install --upgrade pip
+python3 get-pip
 pip install datetime
 pip install hgtk
 pip install six
 pip install statistics
 pip install tqdm
-python3 client.py
+pip install random
+pip install time
+pip install os
+pip install getkey
+pip install keys
