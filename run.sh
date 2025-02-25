50377267a523c3fb4fbc61b55084281af6308edf
p0pl99 <80199718+usercruser@users.noreply.github.com>
2025-02-25 14:42:14 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,21 @@
+sudo apt-get install python3
+sudo zypper install python3
+sudo pacman -Sy python3
+sudo apt-get install python-pip
+sudo zypper install python-pip
+sudo pacman -Sy python-pip
+sudo apt-get install python-xyz
+sudo zypper install python-xyz
+sudo pacman -Sy python-xyz
 python3 -m pip install --upgrade pip
+python3 get-pip
 pip install datetime
 pip install hgtk
 pip install six
 pip install statistics
 pip install tqdm
-python3 client.py
+pip install random
+pip install time
+pip install os
+pip install getkey
+pip install keys
